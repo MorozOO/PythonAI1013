b441769537e6b2bb4949eebe98c7686f2c1a3058
MorozOO <Vx7nneN8508B>
2023-11-09 16:15:41 +0200

    Lesson6 CreateExeptions create def check_material(amoutn_of_material, limit_value)

--- Lesson6/CreateExeptions.py
+++ Lesson6/CreateExeptions.py
@@ -1,3 +1,10 @@
 class BuildingError(Exception):
     def __str__(self):
         return f"Занадто мало матеріалів для того, щоб побудувати будинок"
+def check_material(amoutn_of_material, limit_value):
+    if amoutn_of_material > limit_value:
+        return "матеріалів достатньо"
+    else:
+        raise BuildingError(amoutn_of_material)
+matterial = 100
+print(check_material(matterial, 300))
