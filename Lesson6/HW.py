f8dc36672add8adbf5c608c223dfb57627afedfa
MorozOO <Vx7nneN8508B>
2023-11-16 15:43:52 +0200

    HomeWork6

---
+++ Lesson6/HW.py
@@ -0,0 +1,23 @@
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+# data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+data = {10: 2, 2: 5,  18: 0,  8 : 4}
+
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except TypeError:
+        print("TypeError")
+    except ValueError:
+        print("ValueError")
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+
+
+print(result)
