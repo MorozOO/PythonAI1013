eb74491249a97d50b77f79ca151d04cad41e6b21
MorozOO <Vx7nneN8508B>
2023-11-28 16:53:03 +0200

    Lesson10

--- Lesson8/loging.py
+++ Lesson8/loging.py
@@ -1,7 +1,19 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
+# logging.basicConfig(level=logging.DEBUG)
+# logging.debug("debug")
+# logging.info("info")
+# logging.warning("warning")
+# logging.error("error")
+# logging.critical("critical")
+
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w",
+                    format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+
+#
+# logging.debug("Помикла найнижчого рівня")
+# logging.info("info")
+
+try:
+    print(10/0)
+except Exception:
+    logging.exception("Zero division Error")
