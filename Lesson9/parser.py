fb37a32f9f929109406f94377da9f7e571c240c5
MorozOO <Vx7nneN8508B>
2023-11-28 16:54:10 +0200

    Lesson10(1)

---
+++ Lesson9/parser.py
@@ -0,0 +1,10 @@
+# import urllib.request
+# opener = urllib.request.build_opener()
+# response = opener.open("https://httpbin.org/get")
+# print(response.read())
+
+import requests
+response = requests.get("https://httpbin.org/get", data="Test data",headers={"h1":"Test title"})
+print(response.text)
+# print(response.content)
+# print(f"Datatype - {response.text}")
